6b8cec6e90fa85984780c0edabf1792b565cb611
Joneq <gongyang1217@qq.com>
2019-03-21 19:00:56 +0800

    增加网页的颜色变换

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,10 +22,6 @@
 
             body{
                 background: url(./photo/background/luisa.jpg) no-repeat;
-                width:100%;
-                height:100%;
-                background-size:100% 100%;
-                position:absolute;
             }
 
             .full-height {
